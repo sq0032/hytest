08cd9b9a424e1a37a85d7ecf926fa6028a256aca
sq0032 <sq0032@gmail.com>
2013-08-17 01:09:36 +0800

    working on W8C-3

--- accounts/settings.py
+++ accounts/settings.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'QuickSeller001@gmail.com'
-EMAIL_HOST_PASSWORD = 'movingshop'
+EMAIL_HOST_USER = 'mark.humanwell@gmail.com'
+EMAIL_HOST_PASSWORD = 'squall0032'
 EMAIL_PORT = 587
