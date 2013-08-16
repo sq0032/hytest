4d2606acf20c1d6fad3232436fe253f7f2d86ffc
sq0032 <sq0032@gmail.com>
2013-08-17 01:40:46 +0800

    W8C-3 complete

--- shops/views.py
+++ shops/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 from django.contrib.auth.models import User
 from django.views.decorators.http import require_http_methods, require_GET, require_POST
-
+ 
 from rest_framework.renderers import JSONRenderer
 from rest_framework.views import APIView
 from rest_framework.response import Response
