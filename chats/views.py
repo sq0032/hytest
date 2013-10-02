dc05aa48688064975e51ec9b512369ba87408f29
squall <squall@squall-PC>
2013-10-02 22:53:18 +0800

    1 allow to fetch messages from the server 2 allow to send message to the server

--- chats/views.py
+++ chats/views.py
@@ -17,18 +17,26 @@ from items.models import Item
 
 
 def getList(request):
-    chat = Chat.objects.filter(seller__username=request.user)
-    serializer = ChatSerializer(chat, many=True)
-    json = JSONRenderer().render(serializer.data)
-    return HttpResponse(json)
+	chat = Chat.objects.filter(seller__username=request.user)
+	serializer = ChatSerializer(chat, many=True)
+	json = JSONRenderer().render(serializer.data)
+	print('print json:')
+	print(json)
+	return HttpResponse(json)
 
+@api_view(['GET','POST'])
 def reply(request, chat_id):
-    if request.method == 'POST':
-        return HttpResponse('chat_id='+chat_id)
-    elif request.method == 'GET':
-        reply = Reply.objects.filter(chat__id=chat_id)
-        serializer = ReplySerializer(reply, many=True)
-        json = JSONRenderer().render(serializer.data)
-        return HttpResponse(json)
-        
-        
+	if request.method == 'POST':
+		name = request.user.username
+		user = User.objects.get(username=name)
+		chat = Chat.objects.get(id=chat_id)
+		reply = request.DATA.get('reply')
+		Reply.objects.create(chat=chat, speaker=user, ip='123.123.123.123', reply=reply)
+		return HttpResponse('chat_id='+chat_id)
+	
+	elif request.method == 'GET':
+		reply = Reply.objects.filter(chat__id=chat_id)
+		serializer = ReplySerializer(reply, many=True)
+		json = JSONRenderer().render(serializer.data)
+		return HttpResponse(json)
+
