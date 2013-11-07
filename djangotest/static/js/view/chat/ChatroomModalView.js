06a203af134761cd41ce427c3245f17a4165f434
squall <squall@172.17.158.15>
2013-11-08 02:47:08 +0800

    BW15B complete

--- djangotest/static/js/view/chat/ChatroomModalView.js
+++ djangotest/static/js/view/chat/ChatroomModalView.js
@@ -3,18 +3,19 @@ app.ChatroomModalView = Backbone.View.extend({
 	className:'modal fade',
 	initialize: function(){
 		this.chat = this.model;
-
 	
 		this.initChat();
 		
 		this.listenTo( this.chat.replys, 'reset', this.renderBody);
 		this.listenTo( this.chat.replys, 'add', this.renderReply);
 	},
+
 	events:{
 		'click .modal-footer>button':'sendReply',
 		'click .close':'close',
 		'click .open':'open'
 	},
+	
 	initChat:function(){
 		//Chatroom Modal
 		this.$el.html(
@@ -24,7 +25,7 @@ app.ChatroomModalView = Backbone.View.extend({
 						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>\
 						<h4 class="modal-title">'+this.chat.get('item')+'</h4>\
 					</div>\
-					<div class="modal-body" style="background-color: #E8E8E8">\
+					<div class="modal-body" style="background-color: #E8E8E8;max-height:400px;overflow-y: auto;">\
 						<ul class="msglist">\
 						</ul>\
 					</div>\
@@ -36,13 +37,20 @@ app.ChatroomModalView = Backbone.View.extend({
 			</div>'
 		);
 	},
+	
+	scrollDown:function(){
+		var n = this.$(".msglist")[0].scrollHeight;
+		alert(n);
+		this.$('.modal-body').scrollTop(n);
+		//$('.msglist').scrollTop(8000);	//scroll bar位置還要修改 #BW15B		
+	},
+
 	renderBody:function(){
 		console.log(this.chat.replys.length);
 		$('.msglist').html('');
 		this.chat.replys.each(function(reply){
 			this.renderReply(reply);
 		}, this);
-		$('.msglist').scrollTop(8000);	//scroll bar位置還要修改 #BW15B
 	},
 	renderReply:function(reply){
 		var replyView = new app.ReplyView({model:reply});
@@ -52,8 +60,10 @@ app.ChatroomModalView = Backbone.View.extend({
 		if(typeof reply.get('datetime')=='undefined'){
 			reply.save().fail(function(){
 				reply.trigger('fail');
-			});					
+			});
 		}
+
+		this.scrollDown();
 	},
 	sendReply:function(){
 		if($(".modal-footer>input").val()){
@@ -73,6 +83,7 @@ app.ChatroomModalView = Backbone.View.extend({
 			this.chat.replys.add(reply);
 			$(".modal-footer>input").val('');
 		}
+		
 	},
 
 	close:function(){
@@ -82,5 +93,6 @@ app.ChatroomModalView = Backbone.View.extend({
 	open:function(){
 		this.chat.replys.fetch({reset:true});
 		this.$el.modal('show');
+		this.scrollDown();
 	}
 });
